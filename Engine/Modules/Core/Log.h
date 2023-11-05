ffc2ecad35e4b6ca99e45919617ee50c0382174f
Nox <nox.tux@gmail.com>
2023-11-05 01:04:55 -0500

    fixed a bug where setchannel macro didn't call the static version of the system

--- Engine/Modules/Core/Log.h
+++ Engine/Modules/Core/Log.h
@@ -9,11 +9,9 @@ namespace ECSEngine
 	const std::string_view Log = "Log";
 }
 
-#define _ECSE_GET_LOG_SYSTEM ECSEngine::Application::get().getSystemContainer().getSystem<ECSEngine::LogSystem>().lock()
-
 #define ECSE_LOG_VERBOSE(CHANNELNAME, LOGMESSAGE, ...) LogSystem::s_log(CHANNELNAME, ELogLevel::Verbose, std::format(LOGMESSAGE, __VA_ARGS__))
 #define ECSE_LOG(CHANNELNAME, LOGMESSAGE, ...) LogSystem::s_log(CHANNELNAME, ELogLevel::Log, std::format(LOGMESSAGE, __VA_ARGS__))
 #define ECSE_LOG_WARNING(CHANNELNAME, LOGMESSAGE, ...) LogSystem::s_log(CHANNELNAME, ELogLevel::Warning, std::format(LOGMESSAGE, __VA_ARGS__))
 #define ECSE_LOG_ERROR(CHANNELNAME, LOGMESSAGE, ...) LogSystem::s_log(CHANNELNAME, ELogLevel::Error, std::format(LOGMESSAGE, __VA_ARGS__))
 
-#define SET_CHANNEL_LOG_LEVEL(CHANNELNAME, LOGLEVEL) _ECSE_GET_LOG_SYSTEM->setChannelLogLevel(CHANNELNAME, LOGLEVEL)
+#define SET_CHANNEL_LOG_LEVEL(CHANNELNAME, LOGLEVEL) LogSystem::s_setChannelLogLevel(CHANNELNAME, LOGLEVEL)
