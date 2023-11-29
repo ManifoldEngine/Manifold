815af042b642bfb6b3c649cbe1c32800fd6223cd
Nox <nox.tux@gmail.com>
2023-11-29 00:55:13 -0500

    fixed a grouping issue in the modules

--- Engine/Modules/Camera/premake5.lua
+++ Engine/Modules/Camera/premake5.lua
@@ -1,14 +1,15 @@
 include "../../../locations.lua"
+group "Engine"
+    project "Camera"
+        kind "StaticLib"
+        location (moduledir .. "/%{prj.name}")
+        
+        files { 
+            moduledir .. "/%{prj.name}/**.h",
+            moduledir .. "/%{prj.name}/**.cpp" 
+        }
 
-project "Camera"
-    kind "StaticLib"
-    location (moduledir .. "/%{prj.name}")
-    
-    files { 
-        moduledir .. "/%{prj.name}/**.h",
-        moduledir .. "/%{prj.name}/**.cpp" 
-    }
+        links { "Core", "ECS" }
 
-    links { "Core", "ECS" }
-
-    includedirs { moduledir .. "/**" }
+        includedirs { moduledir .. "/**" }
+group ""
